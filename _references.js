37cc28fc2c13fbd2f94f0e5138b887a4323fc9c5
Hossam <comm.eng.hossam@gmail.com>
2018-01-27 18:29:07 +0200

    Testing Changes

--- _references.js
+++
@@ -1,7 +0,0 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="fonts/icomoon/demo-files/demo.js" />
-/// <reference path="fonts/icomoon/SVG/svgxuse.js" />
-/// <reference path="scripts/bundled/ui-kit.min.js" />
-/// <reference path="scripts/sources/ui-kit.js" />
-/// <reference path="scripts/vendors/bootstrap/bootstrap-select.js" />
-/// <reference path="scripts/vendors/jquery-3.1.1/jquery-3.1.1.js" />
